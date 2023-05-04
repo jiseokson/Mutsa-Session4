c884a02345acf993c4be34db5929b8690cdcf15a
Jiseok Son <jiseokson@gmail.com>
2023-05-05 02:30:14 +0900

    ForeignKey의 속성 변경: DO_NOTHING

--- MutsaWebEx/posts/models.py
+++ MutsaWebEx/posts/models.py
@@ -7,14 +7,14 @@ class User(models.Model):
     nickname = models.CharField(verbose_name='nickname', max_length=20)
 
 class Post(models.Model):
-    writer = models.ForeignKey(verbose_name='writer', to=User, on_delete=models.CASCADE)
+    writer = models.ForeignKey(verbose_name='writer', to=User, on_delete=models.DO_NOTHING)
     title = models.CharField(verbose_name='title', max_length=20)
     content = models.TextField(verbose_name='content')
     view_count = models.IntegerField(verbose_name='view count')
     created_at = models.DateTimeField(verbose_name='created at', auto_now_add=True)
 
 class Comment(models.Model):
-    writer = models.ForeignKey(verbose_name='writer', to=User, on_delete=models.CASCADE)
-    comment_on = models.ForeignKey(verbose_name='comment on', to=Post, on_delete=models.CASCADE)
+    writer = models.ForeignKey(verbose_name='writer', to=User, on_delete=models.DO_NOTHING)
+    comment_on = models.ForeignKey(verbose_name='comment on', to=Post, on_delete=models.DO_NOTHING)
     content = models.TextField(verbose_name='content')
-    created_on = models.DateTimeField(verbose_name='created on')
+    created_at = models.DateTimeField(verbose_name='created at', auto_now_add=True)
